adc5ef1f9f9e038d2643474d6cf047ae3bb31398
ibra1390 <ing.gonzalezvij@hotmail.com>
2025-05-09 12:24:27 -0600

    product changed

--- src/js/product.js
+++ src/js/product.js
@@ -4,9 +4,7 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; 
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
