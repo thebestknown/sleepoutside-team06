8e945bd830f0e0079e8e455b44e55b3824aaa905
ibra1390 <ing.gonzalezvij@hotmail.com>
2025-05-15 17:24:30 -0600

    product.js updated

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,10 +1,4 @@
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error('Bad Response');
-  }
-}
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails  {
   constructor(productId, dataSource){
@@ -13,12 +7,35 @@ export default class ProductDetails  {
     this.dataSource = dataSource;
 }
 
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson).then((data) => data);
+async init() {  
+  this.product = await this.dataSource.findProductById(this.productId);
+  this.renderProductDetails();
+  document.getElementById('addToCart')
+    .addEventListener('click', this.addProductToCart.bind(this));
+}
+
+addProductToCart() {
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(this.product);
+  setLocalStorage("so-cart", cartItems);
+}
+
+renderProductDetails() {
+    productDetailsTemplate(this.product);
   }
-  async findProductById(id) {
-    const products = await this.getData()
-    return products.find((item) => item.Id === id);
-  }
+}
+
+function productDetailsTemplate(product) {
+  document.querySelector('h2').textContent = product.Brand.Name;
+  document.querySelector('h3').textContent = product.NameWithoutBrand;
+
+  const productImage = document.getElementById('productImage');
+  productImage.src = product.Image;
+  productImage.alt = product.NameWithoutBrand;
+
+  document.getElementById('productPrice').textContent = product.FinalPrice;
+  document.getElementById('productColor').textContent = product.Colors[0].ColorName;
+  document.getElementById('productDesc').innerHTML = product.DescriptionHtmlSimple;
+
+  document.getElementById('addToCart').dataset.id = product.Id;
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,12 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 import ProductData from "./ProductData.mjs";
-import {getParam} from "./utils.mjs"
+import { getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
+const productId = getParam("product");
 
-console.log(dataSource.findProductById(productId)); 
+// console.log(dataSource.findProductById(productId));
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+// Call the init() method using the class instance
+product.init();
