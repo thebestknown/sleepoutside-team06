40070cfb42a01638954a7d04878a5432c640e29c
ibra1390 <ing.gonzalezvij@hotmail.com>
2025-05-21 15:05:14 -0600

    team activity 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
