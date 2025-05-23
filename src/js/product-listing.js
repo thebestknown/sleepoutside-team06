a4f948208edf0a57282788f8320751f9576f9e72
ibra1390 <ing.gonzalezvij@hotmail.com>
2025-05-23 14:41:27 -0600

    individual activity week 3 finished

---
+++ src/js/product-listing.js
@@ -0,0 +1,12 @@
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+loadHeaderFooter();
+
+const category = getParam("category");
+const dataSource = new ProductData();
+const listElement = document.querySelector(".product-list");
+const myList = new ProductList(category, dataSource, listElement);
+
+myList.init();
