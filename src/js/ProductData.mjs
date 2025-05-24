9a0894d7fa992398254edbd8651da7f205217516
thebestknown <jeimyespinoza971@gmail.com>
2025-05-24 10:01:04 -0500

    Remove from cart act done

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.VITE_SERVER_URL;
+const baseURL = import.meta.env.VITE_SERVER_URL + '/';
 
 function convertToJson(res) {
   if (res.ok) {
@@ -23,7 +23,7 @@ export default class ProductData {
   return data.Result;
 }
   async findProductById(id) {
-    const response = await fetch(`${baseURL}product/${id}`);
+    const response = await fetch(`${baseURL}/product/${id}`);
     const data = await convertToJson(response);
     console.log(data.Result);
     return data.Result;
