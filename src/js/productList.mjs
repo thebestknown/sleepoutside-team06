c070ac86646d1b8f58ff2c49cd678efe9b2e9d13
ibra1390 <ing.gonzalezvij@hotmail.com>
2025-05-28 19:35:20 -0600

    product List fixed

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,31 +1,35 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-
-    return `<li class="product-card">
-      <<a href="product_pages/?products=${product.Id}">
-        <img src="${product.Image}" alt="${product.Name}">
-        <h2>${product.Brand.Name}</h2>
-        <h3>${product.Name}</h3>
-        <p class="product-card__price">$${product.FinalPrice}</p>
-      </a>
-    </li>`
-  }
-
+  return `<li class="product-card">
+    <a href="product_pages/?product=${product.Id}">
+      <img src="${product.Image}" alt="${product.Name}">
+      <h2 class="card__brand">${product.Brand.Name}</h2>
+      <h3 class="card__name">${product.Name}</h3>
+      <p class="product-card__price">$${product.FinalPrice}</p>
+    </a>
+  </li>`;
+}
 
 export default class ProductList {
-    constructor(category, dataSource,listElement) {
-        this.category = category ;
+    constructor(category, dataSource, listElement) {
+        this.category = category;
         this.dataSource = dataSource;
         this.listElement = listElement;
-        
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+    }
+
+    renderList(list) {
+        // const htmlStrings = list.map(productCardTemplate);
+        // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+
+        renderListWithTemplate(productCardTemplate, this.listElement, list, 'afterbegin', true);
+
+    }
+
+
 }
-async init() {
-    const list = await this.dataSource.getData();
-    this.renderList(list);
-    
-  }
-  renderList(list) {
-    renderListWithTemplate(productCardTemplate, this.listElement, list);
-  }
-}
+
