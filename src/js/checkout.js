8095f0a4c4b2717c86db008124021e9cea4aa242
ibra1390 <ing.gonzalezvij@hotmail.com>
2025-05-29 19:33:59 -0600

    css alert added

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,6 +1,7 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import CheckoutProcess from "./CheckoutProcess.mjs";
 
+
 loadHeaderFooter();
 
 const order = new CheckoutProcess("so-cart", ".checkout-summary");
