95c4b408e4f5e19a83ac887e8dcff0eaa1872114
ibra1390 <ing.gonzalezvij@hotmail.com>
2025-05-31 14:06:15 -0600

    backpack icon animation completed

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,7 +1,6 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import CheckoutProcess from "./CheckoutProcess.mjs";
 
-
 loadHeaderFooter();
 
 const order = new CheckoutProcess("so-cart", ".checkout-summary");
--- src/js/product.js
+++ src/js/product.js
@@ -12,3 +12,10 @@ const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
 // Call the init() method using the class instance
 product.init();
+
+document.querySelector("#add-to-cart").addEventListener("click", () => {
+  const cartIcon = document.querySelector(".cart-icon");
+  cartIcon.classList.remove("animation");
+  void cartIcon.offsetWidth;
+  cartIcon.classList.add("animation");
+});
